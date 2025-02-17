f00a29d403df6d211dbb0645971f31a8d6a42d46
Suhan Muhammed S <suhanmuhammed4@gmail.com>
2025-02-17 17:56:30 +0530

    initial commit

---
+++ src/main/kotlin/com/course/CourseMedia/CourseMediaApplication.kt
@@ -0,0 +1,11 @@
+package com.course.CourseMedia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CourseMediaApplication
+
+fun main(args: Array<String>) {
+	runApplication<CourseMediaApplication>(*args)
+}
---
+++ src/test/kotlin/com/course/CourseMedia/CourseMediaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.course.CourseMedia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseMediaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
